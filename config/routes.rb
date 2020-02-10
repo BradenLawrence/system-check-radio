025f3cfb3b44b80bd0690b4cc00d134ee30bab7a
Braden Lawrence <b@bradenlawrence.com>
2020-02-10 14:52:18 -0500

    add api controller for playlists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root "static_pages#index"
   devise_for :users
 
-  get '/playlists', to: 'static_pages#index'
+  get "/playlists", to: "static_pages#index"
+
+  namespace "api" do
+    namespace "v1" do
+      resources :playlists, only: [:index]
+    end
+  end
 end
