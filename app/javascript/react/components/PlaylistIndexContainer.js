08a74783540bde0923bf171d6df1d5d2e5db257c
Braden Lawrence <b@bradenlawrence.com>
2020-02-10 13:50:58 -0500

    add fetch call to retrieve playlists and store it in state

--- app/javascript/react/components/PlaylistIndexContainer.js
+++ app/javascript/react/components/PlaylistIndexContainer.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
 
 const PlaylistIndexContainer = (props) => {
+  const defaultPlaylist = {
+    name: "",
+    submissions: []
+  }
+  const [playlist, setPlaylist] = useState(defaultPlaylist)
+
+  useEffect(() => {
+    fetch("/api/v1/playlists")
+    .then(response => {
+      if(response.ok) {
+        return response.json
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`)
+      }
+    })
+    .then(json => setPlaylist(json))
+  }, [])
+
   return(
     <h1>Welcome to the Playlist Index!</h1>
   )
