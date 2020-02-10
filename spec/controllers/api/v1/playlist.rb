0d0ae5f20a0e5dc03eea4d6a9403b98d471418af
Braden Lawrence <b@bradenlawrence.com>
2020-02-10 18:58:52 -0500

    add test for presence of a submission in playlist

--- spec/controllers/api/v1/playlist.rb
+++ spec/controllers/api/v1/playlist.rb
@@ -4,6 +4,11 @@ RSpec.describe Api::V1::PlaylistsController, type: :controller do
   describe "GET#index" do
     let!(:playlist1) { Playlist.create(name: "Technofolk")}
     let!(:playlist2) { Playlist.create(name: "Heavy Metal Jazz")}
+    let!(:submission1) { Submission.create(
+      track: "Saxophone Thrash",
+      description: "Gnarly stuff",
+      playlist: playlist2
+    )}
 
     it "should return the most recently created playlist" do
       get :index
@@ -16,6 +21,7 @@ RSpec.describe Api::V1::PlaylistsController, type: :controller do
       expect(returned_json.class).to_not eq(Array)
 
       expect(returned_json["name"]).to eq("Heavy Metal Jazz")
+      expect(returned_json["submissions"].first["track"]).to eq("Saxophone Thrash")
     end
   end
 end
