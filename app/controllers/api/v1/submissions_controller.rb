75994a4093a25c9882ba48e7e6cd146f91fb120b
Braden Lawrence <b@bradenlawrence.com>
2020-02-12 16:53:33 -0500

    add most recent playlist to a new submission's params and return as json

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -2,16 +2,23 @@ class Api::V1::SubmissionsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    submission = create_params
-    binding.pry
-    
+    submission = Submission.new(create_params)
+    submission.playlist = Playlist.last
+
+    if submission.save
+      render json: submission.playlist
+    else
+      render json: submission.errors.full_messages
+    end
+
   end
 
   private
 
   def create_params
     params.require(:submissionData).permit(
-      :name, :image, :album, :artists, :description, :preview_url, :external_url
+      :album, :artists, :description, :duration_ms, :external_url, :image,
+      :name, :preview_url
     )
   end
 end
