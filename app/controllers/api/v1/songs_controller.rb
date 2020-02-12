c72692678351dfd9613ca66c50fe1a87bdb0cead
Braden Lawrence <b@bradenlawrence.com>
2020-02-11 20:13:53 -0500

    refactor search controller to parse spotify data in a wrapper PORO

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,13 +1,10 @@
-require 'rspotify'
-
 class Api::V1::SongsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
-  RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
 
   def search
     query = query_params
-    results = RSpotify::Track.search(query["term"])
-    render json: results
+    results = TracksWrapper.retrieve_tracks(query["term"])
+    render json: results.track_data
   end
 
   private
