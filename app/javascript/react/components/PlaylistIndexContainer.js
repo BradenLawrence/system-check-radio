4cb71d07269fca885473b45d3c04d0e116824488
Braden Lawrence <b@bradenlawrence.com>
2020-02-12 16:54:12 -0500

    handle posting submission data from child component

--- app/javascript/react/components/PlaylistIndexContainer.js
+++ app/javascript/react/components/PlaylistIndexContainer.js
@@ -28,10 +28,45 @@ const PlaylistIndexContainer = (props) => {
     setSearchResults(results)
   }
 
+  const handleCreateSubmission = (submissionData) => {
+    let body = new FormData()
+    body.append("submissionData[album]", submissionData.album)
+    body.append("submissionData[artists]", submissionData.artists.join(", "))
+    body.append("submissionData[description]", submissionData.description)
+    body.append("submissionData[duration_ms]", submissionData.duration_ms)
+    body.append("submissionData[external_url]", submissionData.external_url)
+    body.append("submissionData[image]", submissionData.image)
+    body.append("submissionData[name]", submissionData.name)
+    body.append("submissionData[preview_url]", submissionData.preview_url)
+    fetch("/api/v1/submissions", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Accept": "application/json"
+      },
+      body: body
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        throw new Error(response.status + ": " + response.statusText)
+      }
+    })
+    .then(json => {
+      setPlaylist(json)
+      setSearchResults([])
+    })
+    .catch(error => console.error("Error searching tracks: " + error.message))
+  }
+
   const searchResultsList = searchResults.map(result => {
     return(
       <li key={result.id}>
-        <SearchResultTile result={result} />
+        <SearchResultTile
+          result={result}
+          handleCreateSubmission={handleCreateSubmission}
+        />
       </li>
     )
   })
