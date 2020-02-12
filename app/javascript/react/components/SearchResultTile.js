49a39c473fdabff4a3afdcdc86053eff660ddd02
Braden Lawrence <b@bradenlawrence.com>
2020-02-12 14:25:06 -0500

    add a form to create new submissions

--- app/javascript/react/components/SearchResultTile.js
+++ app/javascript/react/components/SearchResultTile.js
@@ -1,16 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchResultTile = ({ result} ) => {
+const SearchResultTile = ({ result } ) => {
+  const defaultSubmission = {
+    ...result,
+    description: ""
+  }
+  const [submissionData, setSubmissionData] = useState(defaultSubmission)
+
+  const handleInput = (event) => {
+    let key = event.currentTarget.name
+    let value = event.currentTarget.value
+    setSubmissionData({
+      ...submissionData,
+      [key]: value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    debugger
+    event.preventDefault()
+    let body = new FormData()
+    body.append("submissionData[image]", submissionData.image)
+    body.append("submissionData[name]", submissionData.name)
+    fetch("/api/v1/submissions", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Accept": "application/json"
+      },
+      body: body
+    })
+  }
+
   return(
-    <div className="row search-result">
-      <div className="medium-2 columns album-cover">
-        <img src={result.image}/>
+    <div>
+      <div className="row search-result">
+        <div className="small-2 columns album-cover">
+          <img src={result.image}/>
+        </div>
+        <ul className="small-10 columns track-info">
+          <li><h3>{result.name}</h3></li>
+          <li className="album-title">{result.album}</li>
+          <li className="artist-name">{result.artists.join(", ")}</li>
+        </ul>
+        <div className="small-12 columns result-actions">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Why are you nominating this song?
+              <input
+                type="text"
+                name="description"
+                onChange={handleInput}
+                placeholder="Ex: How I felt writing feature tests this morning."
+                value={submissionData.description}
+              />
+            </label>
+            <input className="button" type="button" value="Submit" />
+          </form>
+        </div>
       </div>
-      <ul className="medium-10 columns track-info">
-        <li><h3>{result.name}</h3></li>
-        <li className="album-title">{result.album}</li>
-        <li className="artist-name">{result.artists.join(", ")}</li>
-      </ul>
     </div>
   )
 }
