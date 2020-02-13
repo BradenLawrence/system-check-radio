465c5f3ce5d5a760fd514f53eb43f6244e31fbfc
Braden Lawrence <b@bradenlawrence.com>
2020-02-13 15:53:40 -0500

    add inputs and buttons for editing submissions

--- app/javascript/react/components/SubmissionTile.js
+++ app/javascript/react/components/SubmissionTile.js
@@ -1,6 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SubmissionTile = ({ submission }) => {
+  const [descriptionInput, setDescriptionInput] = useState(submission.description)
+
+  const handleInputChange = (event) => {
+    setDescriptionInput(event.currentTarget.value)
+  }
+
+  const handleEditSave = (event) => {
+    event.preventDefault()
+    console.log('Edit Saved')
+  }
+
+  const handleCancelEdit = (event) => {
+    console.log('cancel edit')
+  }
+
+  const handleDelete = (event) => {
+    event.preventDefault()
+    console.log('Delete Submission')
+  }
+
+  const editButton = (
+    <div>
+      <li className="submission-edit"><i className="fa fa-pencil"></i></li>
+      <li className="submission-cancel">
+        <button
+          type="button"
+          onClick={handleCancelEdit}
+        >
+          <i className="fa fa-pencil"></i>
+        </button>
+      </li>
+    </div>
+  )
+
+  const descriptionArea = (
+    <li className="submission-description">"{submission.description}"</li>
+  )
+
+  const editForm = (
+    <li className="submission-edit-form">
+      <form onSubmit={ handleEditSave }>
+        <input
+          name="description"
+          type="text"
+          value={descriptionInput}
+          onChange={handleInputChange}
+        />
+        <input
+          type="submit"
+          value="Save Changes"
+        />
+        <input
+          type="submit"
+          value="Delete Submission"
+          onClick={handleDelete}
+        />
+      </form>
+    </li>
+  )
+
   return(
     <div className="submission-listing">
       <div className="row">
@@ -19,8 +79,10 @@ const SubmissionTile = ({ submission }) => {
           <i className="fa fa-thumbs-up"></i>
           <i className="fa fa-thumbs-down"></i>
         </div>
-        <ul className="small-9 medium-10 columns">
-          <li className="submission-description">"{submission.description}"</li>
+        <ul className="submission-user-content small-9 medium-10 columns">
+          { editButton }
+          { descriptionArea }
+          { editForm }
         </ul>
       </div>
     </div>
