92eda53216eb54eaf5910eea18c4f3fe1ca07ad1
Braden Lawrence <b@bradenlawrence.com>
2020-02-12 21:04:12 -0500

    update enzyme test for SubmissionTiles

--- app/javascript/react/components/SubmissionTile.test.js
+++ app/javascript/react/components/SubmissionTile.test.js
@@ -9,19 +9,30 @@ Enzyme.configure({ adapter: new Adapter() })
 describe("SubmissionTile", () => {
   let wrapper
   let sub = {
-    track: "We All Become",
-    description: "Fav song from Transistor."
+    name: "Africa",
+    artists: "TOTO",
+    album: "Toto IV",
+    preview_url: "https://p.scdn.co/mp3-preview/dd78dafe31bb98f230372c038a126b8808f9349b?cid=c205b06529104ab6a0d1148d267b56cf",
+    description: "go"
   }
 
   beforeEach(() => {
     wrapper = mount(<SubmissionTile submission={ sub } />)
   })
 
-  it("should render a track-name element containing the title of the track", () => {
-    expect(wrapper.find("li.track-name").text()).toBe("Track name: We All Become")
+  it("should render a submission-name element containing the title of the track", () => {
+    expect(wrapper.find("li.submission-name").text()).toBe("Africa")
   })
 
-  it("should render a track-description containing a description of the track", () => {
-    expect(wrapper.find("li.track-description").text()).toBe("Description: Fav song from Transistor.")
+  it("should render a submission-description containing a description of the track", () => {
+    expect(wrapper.find("li.submission-description").text()).toBe('"go"')
+  })
+
+  it("should render a submission-details containing a description of the track", () => {
+    expect(wrapper.find("li.submission-details").text()).toBe("Toto IV | TOTO")
+  })
+
+  it("should render a link pointing to a preview URL", () => {
+    expect(wrapper.find("a").prop("href")).toBe("https://p.scdn.co/mp3-preview/dd78dafe31bb98f230372c038a126b8808f9349b?cid=c205b06529104ab6a0d1148d267b56cf")
   })
 })
