b7e45373601db8d16c13b752d5e870308dd498ac
Braden Lawrence <b@bradenlawrence.com>
2020-02-14 17:52:25 -0500

    add route and controller for destroying submissions

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       resources :playlists, only: [:index]
-      resources :submissions, only: [:create, :update]
+      resources :submissions, only: [:create, :update, :destroy]
       post "/songs", to: "songs#search"
     end
   end
