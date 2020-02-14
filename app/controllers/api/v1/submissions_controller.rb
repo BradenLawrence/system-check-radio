fd7b578bd65a197ad1d340cc808387ce79ceeb07
Braden Lawrence <b@bradenlawrence.com>
2020-02-14 13:45:12 -0500

    add route and controller for updating submissions

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -2,23 +2,34 @@ class Api::V1::SubmissionsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    submission = Submission.new(create_params)
+    submission = Submission.new(submission_params)
     submission.playlist = Playlist.last
 
     if submission.save
       render json: submission.playlist
     else
-      render json: {errors: submission.errors.full_messages}
+      render json: { errors: submission.errors.full_messages }
+    end
+  end
+
+  def update
+    changes = submission_params
+    submission = Submission.find(changes["id"])
+
+    if submission.update_attributes(changes)
+      render json: submission
+    else
+      render json: { errors: submission.errors.full_messages }
     end
   end
 
   private
 
-  def create_params
+  def submission_params
     params.require(:submissionData).permit(
-      :album, :artists, :description, :duration_ms, :external_url, :image,
-      :name, :preview_url, :track_id
+      :id, :album, :artists, :description, :duration_ms,
+      :external_url, :image, :name, :preview_url, :track_id
     )
   end
-  
+
 end
