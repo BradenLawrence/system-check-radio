3d0ccecca3248eaf0b297e298a74f7f19951f9ab
Braden Lawrence <b@bradenlawrence.com>
2020-02-16 11:29:21 -0500

    add model validation tests for name and roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_valid(:email).when("buddy@example.com") }
   it { should_not have_valid(:email).when(nil, "") }
+
+  it { should have_valid(:name).when("WeBeJammin") }
+  it { should_not have_valid(:name).when(nil, "") }
+
+  it { should have_valid(:role).when(0, 1) }
+  it { should define_enum_for(:role).with_values([:user, :admin]) }
 end
