8b0e50786387f757f5427ecbfc154e26b28f5626
Braden Lawrence <b@bradenlawrence.com>
2020-02-15 20:10:16 -0500

    add name to users migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   validates :role, presence: true
   enum role: { user: 0, admin: 1 }
   after_initialize :set_default_role, if: :new_record?
