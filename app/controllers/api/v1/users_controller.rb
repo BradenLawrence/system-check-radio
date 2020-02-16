92cad2c9dc229366b3c82dc64cbe6ac925e1ba13
Braden Lawrence <b@bradenlawrence.com>
2020-02-15 20:30:58 -0500

    replace ruby controller helpers with react api helpers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
   before_action :authenticate_user!, only: [:show]
-  before_action :user_params, only: [:show]
 
   def index
-
+    render json: User.all
   end
 
   def show
-
-  end
-
-  private
-
-  def user_params
-    @user = User.find(params[:id])
+    render json: User.find()
   end
 end
