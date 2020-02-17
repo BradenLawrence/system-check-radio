4bf428c3a0c48bbbef826135af267f6717375939
Braden Lawrence <b@bradenlawrence.com>
2020-02-17 17:12:01 -0500

    add route and controller for user show page

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,21 @@ class Api::V1::UsersController < ApplicationController
     if current_user.role == "admin"
       render json: User.all
     else
-      render json: { error: "You are not authorized to view this page" }
+      render json: { errors: ["You are not authorized to view this page"] }
     end
+  end
+
+  def show
+    if current_user.role == "admin" || current_user.id == user_params.to_i
+      render json: User.find(user_params)
+    else
+      render json: { errors: ["You are not authorized to view this page"] }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     namespace "v1" do
       resources :playlists, only: [:index]
       resources :submissions, only: [:create, :update, :destroy]
-      resources :users, only: [:index]
+      resources :users, only: [:index, :show]
       post "/songs", to: "songs#search"
     end
   end
