2e48557e569681966be195952054e3e90a138f84
Braden Lawrence <b@bradenlawrence.com>
2020-02-17 11:21:50 -0500

    add foreign key to submissions for a user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   enum role: { user: 0, admin: 1 }
   after_initialize :set_default_role, if: :new_record?
 
+  has_many :submissions
+
   private
 
   def set_default_role
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_003631) do
+ActiveRecord::Schema.define(version: 2020_02_16_213401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,9 @@ ActiveRecord::Schema.define(version: 2020_02_16_003631) do
     t.bigint "playlist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["playlist_id"], name: "index_submissions_on_playlist_id"
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_02_16_003631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submissions", "users"
 end
