73117b5984f1be8deda21c264dbf333916ddb5fb
Braden Lawrence <b@bradenlawrence.com>
2020-02-17 17:14:57 -0500

    add isCurrentUser helper to user serializer for author display

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
+  attributes :id, :name, :email, :isCurrentUser
 
   has_many :submissions, each_serializer: SubmissionSerializer
+
+  def isCurrentUser
+    object == current_user
+  end
 end
