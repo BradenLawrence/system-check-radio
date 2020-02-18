b1ff9d915e63b6fd7821d029ac384b8a89261ef6
Braden Lawrence <b@bradenlawrence.com>
2020-02-18 12:03:16 -0500

    add update route and logic for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,16 +10,33 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    if current_user.role == "admin" || current_user.id == user_params.to_i
-      render json: User.find(user_params)
+    if current_user.role == "admin" || current_user.id == show_params.to_i
+      render json: User.find(show_params)
     else
       render json: { errors: ["You are not authorized to view this page"] }
     end
   end
 
+  def update
+    if current_user.role == "admin"
+      user = User.find(update_params["id"])
+      if user.update_attributes(update_params)
+        render json: user
+      else
+        render json: { errors: user.errors.full_messages }
+      end
+    else
+      render json: { errors: "You are not authorized to edit users" }
+    end
+  end
+
   private
 
-  def user_params
+  def show_params
     params.require(:id)
+  end
+
+  def update_params
+    params.require(:user).permit(:id, :member)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     namespace "v1" do
       resources :playlists, only: [:index]
       resources :submissions, only: [:create, :update, :destroy]
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :update]
       post "/songs", to: "songs#search"
     end
   end
