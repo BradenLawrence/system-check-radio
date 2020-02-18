43e7c276eaffb4448aea180a8f5b374b21e2befd
Braden Lawrence <b@bradenlawrence.com>
2020-02-18 12:03:51 -0500

    add new user attributes to users for checking membership

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,17 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :isCurrentUser
+  attributes :id, :name, :email, :isCurrentUser, :role, :member, :isAdmin
 
   has_many :submissions, each_serializer: SubmissionSerializer
 
   def isCurrentUser
     object == current_user
+  end
+
+  def isAdmin
+    if current_user
+      current_user.role == "admin"
+    else
+      false
+    end
   end
 end
