5638658322acc26ef395aa352acc175790cd7754
Braden Lawrence <b@bradenlawrence.com>
2020-02-23 23:11:24 -0500

    add new show route for displaying the greatest hits playlist

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,6 +2,16 @@ class Api::V1::PlaylistsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
-    render json: Playlist.last
+    render json: Playlist.where(compilation: false).last
+  end
+
+  def show
+    render(
+      json: Playlist.find_or_create_by(
+        name: "Greatest Hits",
+        compilation: true
+      ),
+      serializer: HitsSerializer
+    )
   end
 end
