9726e71efa124bb8ed8c2d9b5d613f6778c7950a
Braden Lawrence <b@bradenlawrence.com>
2020-03-02 23:22:19 -0500

    fix bug where an empty playlist causes hits serializer to fail

--- app/serializers/hits_serializer.rb
+++ app/serializers/hits_serializer.rb
@@ -2,7 +2,7 @@ class HitsSerializer < ActiveModel::Serializer
   attributes :id, :name, :compilation, :submissions
 
   def submissions
-    playlists = Playlist.where(compilation: false)
+    playlists = Playlist.where(compilation: false).where().not(top_submission:false)
     top_submissions = playlists.map do |playlist|
       Submission.find(playlist.top_submission)
     end
